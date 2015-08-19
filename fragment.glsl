5d30a1852d8335726a107316d12b9637d09c1828
Chris Samuelson <chris.sam55@gmail.com>
2015-08-18 21:10:07 -0400

    Added diffuse lighting.

--- fragment.glsl
+++ fragment.glsl
@@ -2,6 +2,8 @@
 
 in vec3 ourColor;
 in vec2 TexCoord;
+in vec3 Normal;
+in vec3 FragPos;
 
 out vec4 color;
 
@@ -9,6 +11,7 @@ uniform sampler2D ourTexture0;
 uniform sampler2D ourTexture1;
 uniform vec3 lightColor;
 uniform vec3 objColor;
+uniform vec3 lightPos;
 
 void main(){
   //combine color and texture data
@@ -16,9 +19,15 @@ void main(){
   //combine 2 textures
 	//color = mix(texture(ourTexture0, TexCoord), texture(ourTexture1, TexCoord), 0.2);
   
+  vec3 norm = normalize(Normal);
+  vec3 lightDir = normalize(lightPos - FragPos);
+  
+  float diff = max(dot(norm, lightDir), 0.0);
+  vec3 diffuse = diff * lightColor;
+  
   float ambientStrength = 0.1f;
   vec3 ambient = ambientStrength * lightColor;
   
-  vec3 result = ambient * objColor;
+  vec3 result = (ambient + diffuse) * objColor;
   color = vec4(result, 1.0f);
 }
--- vertex.glsl
+++ vertex.glsl
@@ -5,16 +5,17 @@ layout (location = 1) in vec3 color;
 layout (location = 2) in vec2 texCoord;
 layout (location = 3) in vec3 normal;
 
-//out vec3 objColor;
 out vec2 TexCoord;
+out vec3 Normal;
+out vec3 FragPos;
 
 uniform mat4 transform;
 uniform mat4 view;
 uniform mat4 projection;
-uniform vec3 lightPos;
 
 void main(){
   gl_Position = projection * view * transform * vec4(position, 1.0);
-  //objColor = color;
   TexCoord = texCoord;
+  FragPos = vec3(transform * vec4(position, 1.0f));
+  Normal = normal;
 }
